b35f6c4aa52cca71bba2a8fb174c9abd7ac09637
nn2331230-create <nn2331230@gmail.com>
2025-11-11 10:57:19 +0530

    Create airt.py

---
+++ airt.py
@@ -0,0 +1,15 @@
+
+age=int(input("enter the age"))
+weight=int(input("enter the weight"))
+print(age+weight)
+print(age-weight)
+print(age*weight)
+print(age/weight)
+print(age//weight)
+print(age%weight)er the age5
+enter the weight2
+7
+3
+10
+2.5
+2
