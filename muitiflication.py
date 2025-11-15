070b1313134b333d721c430ae087c14ad202b4b2
nn2331230-create <nn2331230@gmail.com>
2025-11-15 09:15:52 +0530

    Create muitiflication.py

---
+++ muitiflication.py
@@ -0,0 +1,17 @@
+muitiplication
+num=int(input("enter the number"))
+for i in range(1 ,11):
+  print(f"{num} x {i} ={num * i }")
+
+enter the number5
+5 x 1 =5
+5 x 2 =10
+5 x 3 =15
+5 x 4 =20
+5 x 5 =25
+5 x 6 =30
+5 x 7 =35
+5 x 8 =40
+5 x 9 =45
+5 x 10 =50
+
